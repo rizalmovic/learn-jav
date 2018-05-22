fe00d9de14ba61282241f7b76093dfd2d6e53d29
Habli Muhammad Rizal <Habli_MR349@mitrais.com>
2018-05-22 10:47:47 +0700

    'update'

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World"); // show something
+        List<Integer> rnum = getLists(10);        
+        rnum.forEach((v) -> {
+            System.out.println(v);
+        });        
+    }
+
+    public static List<Integer> getLists (Integer member) {
+        List<Integer> lists = new ArrayList<Integer>();
+        Random generator = new Random();
+        lists = generator.ints(member, 1, 10).boxed().collect(Collectors.toList());
+        return lists;
     }
 }
